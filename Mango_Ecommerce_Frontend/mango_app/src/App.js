f2d82c2ccd48f75ade980689bb0b093857709961
Sanket Borate <104027437+SanketBorate@users.noreply.github.com>
2022-09-23 23:42:09 +0530

    Updated App.js

--- Mango_Ecommerce_Frontend/mango_app/src/App.js
+++ Mango_Ecommerce_Frontend/mango_app/src/App.js
@@ -9,11 +9,29 @@ import Login from './pages/login';
 
 //import S1 from './pages/s1';
 import Home from './pages/home';
-import SignIn from './Signin/SignIn';
+
+import AdminHome from './Dashboard/AdminHome';
+import FarmerHome from './Dashboard/FarmerHome';
 
 
 
-
+import Admin from './Signin/AdminSignIn';
+import Farmer from './Signin/FarmerSignIn';
+import Customer from './Signin/CustomerSignIn';
+import FarmerSignUp from './Signup/FarmerSignUp';
+import CustomerSignUP from './Signup/CustomerSignUp';
+import ViewProducts from './AdminFunction/ViewProducts';
+import AddProduct from './AddProduct/AddProduct';
+import ViewCustomer from './AdminFunction/ViewCustomer';
+import ViewFarmer from './AdminFunction/ViewFarmer';
+import Logout from './Logout/logout';
+import CustomerHome from './Dashboard/CustomerHome';
+import AddtoCart from './Cart/AddtoCart';
+import FarmerViewProduct from './AddProduct/FarmerViewProduct';
+import MangoArraylist from './Products/MangoArraylist';
+import Mangolist from './Products/Mangolist';
+import Cart from './Cart/Cart';
+import Orderdetailsforfarmer from './Order/Orderdetailsforfarmer';
 
  function App() {
   return (
@@ -23,26 +41,49 @@ import SignIn from './Signin/SignIn';
          <BrowserRouter >
            <Routes>
           {/* <Route path="/" element={<Home2/>}> */}
-          <Route path="/" element={<Home/>}>
+          <Route path="/" element={<Home/>}></Route >
           <Route path="/about" element={<AboutUs />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/ourProduct" element={<OurProduct />} />
           <Route path="/login" element={<Login/>} />
-          <Route path="/SignIn" element={<SignIn/>} />
-         
-          </Route >
+
+
+          <Route path="/CustomerSignIn" element={<Customer/>} />
+          <Route path="/AdminSignIn" element={<Admin/>} />
+          <Route path="/FarmerSignIn" element={<Farmer/>} />
+
+          <Route path="/CustomerSignUp"element={<CustomerSignUP/>}/>
+          <Route path="/ViewProducts" element={<ViewProducts/>} />
+
+          <Route path="/FarmerSignUp"element={<FarmerSignUp/>}/>
           
+          <Route path="/AdminHome" element={<AdminHome/>} />
+          <Route path="/FarmerHome" element={<FarmerHome/>} />
+          <Route path="/CustomerHome" element={<CustomerHome/>} />
           
+          <Route path="/AddProduct" element={<AddProduct/>} />
+          <Route path="/ViewCustomer" element={<ViewCustomer/>} />
+          <Route path="/ViewFarmer" element={<ViewFarmer/>} />
+          <Route path="/FarmerViewProduct" element={<FarmerViewProduct/>} />
           
+          <Route path="/AddtoCart" element={<AddtoCart/>} />
+          <Route path="/Cart" element={<Cart/>} />
+          <Route path="/Mangolist" element={<Mangolist/>} />
+          <Route path="/logout" element={<Logout/>} />
+          <Route path="/Orderdetailsforfarmer" element={<Orderdetailsforfarmer/>}></Route>
           </Routes>
-          <Outlet></Outlet>
-          </BrowserRouter>
           
+          </BrowserRouter>
+         {/* <AddProduct></AddProduct> */}
          
           {/*<Footer></Footer>*/}
+
+          {/* <Mangolist></Mangolist> */}
+          
           </div>
 
   );
 }
 
 export default App;
+
