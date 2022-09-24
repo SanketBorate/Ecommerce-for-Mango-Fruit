a4552cf0e72e5f13b836b411168c3e05c59a0b08
Rajratna21 <rajratnagavhane97@gmail.com>
2022-09-24 13:08:32 +0530

    Backend with required changes

---
+++ mangoproductdemo/src/main/java/study/mangodemo/MangoproductdemoApplication.java
@@ -0,0 +1,15 @@
+package study.mangodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+@CrossOrigin
+@SpringBootApplication
+public class MangoproductdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangoproductdemoApplication.class, args);
+	}
+
+}
+ 
