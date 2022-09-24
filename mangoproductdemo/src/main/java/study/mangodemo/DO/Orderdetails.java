70d3fa30a0ca42cfeb992c87badf65a3a5304261
Kumbhar123 <106907323+Kumbhar123@users.noreply.github.com>
2022-09-24 13:43:36 +0530

    Order_Details

---
+++ mangoproductdemo/src/main/java/study/mangodemo/DO/Orderdetails.java
@@ -0,0 +1,47 @@
+package study.mangodemo.DO;
+
+public class Orderdetails {
+
+	private String customername;
+	
+	private String mangoname;
+	
+	private int quantity;
+	
+	private String address;
+
+	public String getCustomername() {
+		return customername;
+	}
+
+	public void setCustomername(String customername) {
+		this.customername = customername;
+	}
+
+	public String getMangoname() {
+		return mangoname;
+	}
+
+	public void setMangoname(String mangoname) {
+		this.mangoname = mangoname;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	
+	
+}
