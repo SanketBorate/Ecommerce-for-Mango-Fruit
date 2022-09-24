da6d14c634d021f1908662e1d5d38495f0e7f64a
pradnyil07 <106861086+pradnyil07@users.noreply.github.com>
2022-09-24 21:03:52 +0530

    Update FarmerInfo.java

--- mangoproductdemo/src/main/java/study/mangodemo/entity/FarmerInfo.java
+++ mangoproductdemo/src/main/java/study/mangodemo/entity/FarmerInfo.java
@@ -150,48 +150,7 @@ public class FarmerInfo {
 	
 	
 	
-	
-//	public FarmerInfo(int farmer_id, String first_name, String last_name, String email_id, String contact_no,
-//			String username, String password) {
-//		super();
-//		this.farmer_id = farmer_id;
-//		this.first_name = first_name;
-//		this.last_name = last_name;
-//		this.email_id = email_id;
-//		this.contact_no = contact_no;
-//		//this.username = username;
-//		this.password = password;
-//	}
 
-
-//	public int getFarmer_id() {
-//		return farmer_id;
-//	}
-//
-//
-//	public void setFarmer_id(int farmer_id) {
-//		this.farmer_id = farmer_id;
-//	}
-//
-//
-//	public String getFirst_name() {
-//		return first_name;
-//	}
-//
-//
-//	public void setFirst_name(String first_name) {
-//		this.first_name = first_name;
-//	}
-//
-//
-//	public String getLast_name() {
-//		return last_name;
-//	}
-//
-//
-//	public void setLast_name(String last_name) {
-//		this.last_name = last_name;
-//	}
 //
 //
 //	public String getEmail_id() {
