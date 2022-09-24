f5d3701fad72f4566d1ea672f5cf67f312c07b05
Kumbhar123 <106907323+Kumbhar123@users.noreply.github.com>
2022-09-24 13:42:05 +0530

    Farmer_Do file added

---
+++ mangoproductdemo/src/main/java/study/mangodemo/DO/FarmerInfoDO.java
@@ -0,0 +1,54 @@
+package study.mangodemo.DO;
+
+public class FarmerInfoDO {
+
+	private String emailid;
+	
+	
+	private String password;
+
+
+
+
+
+//	public String getEmail_id() {
+//		return email_id;
+//	}
+//
+//
+//	public void setEmail_id(String email_id) {
+//		this.email_id = email_id;
+//	}
+//
+//
+//	public FarmerInfoDO(String email_id, String password) {
+//		super();
+//		this.email_id = email_id;
+//		this.password = password;
+//	}
+
+
+	public String getEmailid() {
+		return emailid;
+	}
+
+
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+	
+}
