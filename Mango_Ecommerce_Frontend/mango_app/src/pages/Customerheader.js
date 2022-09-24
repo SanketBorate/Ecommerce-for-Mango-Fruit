7b48bf4f57dcf47229fd7efce5cf70d70a03701c
Shivani Naiknavare <106859063+shivanin14@users.noreply.github.com>
2022-09-25 00:18:07 +0530

    Added Customer Header

---
+++ Mango_Ecommerce_Frontend/mango_app/src/pages/Customerheader.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+
+import { Link } from "react-router-dom";
+export default function Customerheader(){
+    return(
+      <header >
+      
+      <div className="header">
+         <div className="container-fluid">
+            <div className="row">
+               <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                  <div className="full">
+                     <div className="center-desk">
+                        <div className="logo">
+                           <a href="index.html"><img src="../assets/images/logo.png" alt="#" /></a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+                     </div>
+                  
+               </div>
+            </div>
+         
+      
+   
+      </header>
+    )
+}
