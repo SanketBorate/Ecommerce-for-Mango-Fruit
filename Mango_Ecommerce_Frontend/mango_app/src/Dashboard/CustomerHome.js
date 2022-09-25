58de375426837a9dd4ef700f32b6a27b13eceaab
Shivani Naiknavare <106859063+shivanin14@users.noreply.github.com>
2022-09-25 09:15:13 +0530

    New CustomerHome

--- Mango_Ecommerce_Frontend/mango_app/src/Dashboard/CustomerHome.js
+++ Mango_Ecommerce_Frontend/mango_app/src/Dashboard/CustomerHome.js
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 function CustomerHome() {
   var customer=JSON.parse(sessionStorage.getItem("custlogin")) 
   // var email= sessionStorage.getItem('email');
-   let navigate=useNavigate();
+   let navigate=useNavigate(); 
   // console.log(email);
    useEffect(()=>{
       if(customer===null){
